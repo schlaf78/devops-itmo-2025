9562048c9da52e01e858da33aaa8d81de5223947
Mikhail Burmistrov <schlaf2003@mail.ru>
2025-07-08 16:54:05 +0300

    labs 08.07.2025

--- Python/Laboratory-Work/2025.07.01/lab-p4.py
+++ Python/Laboratory-Work/2025.07.01/lab-p4.py
@@ -1,3 +1,14 @@
 
 #Iterations count
-#1
+#1,2,3
+n = int(input('Задайте число '))
+k = 0
+Transit = n
+while n > 0:
+    n = n//10 # Отбрасывание последней цифры числа n
+    k = k + 1
+print("Количество цифр в числе", k)
+#Numbers count
+print("Digits summ is:",sum(int(d) for d in str(Transit)))
+
+#Max disributor finding
