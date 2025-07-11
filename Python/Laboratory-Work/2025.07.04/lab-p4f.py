ac43616b85d085a1d18fa5fe9d6cf49e57ed86ba
Mikhail Burmistrov <schlaf2003@mail.ru>
2025-07-12 02:54:39 +0300

    lab p4f

--- Python/Laboratory-Work/2025.07.04/lab-p4f.py
+++ Python/Laboratory-Work/2025.07.04/lab-p4f.py
@@ -1,13 +1,34 @@
 import os
 
-#print(os.getcwd())
-#os.mkdir('demo_dir')
-#os.listdir(path=".")
-Folder = os.getcwd()
-Asw = set()
-Search = input ("Enter any text:")
+folder = os.getcwd()
+asw = set()
+search = input("Enter any text: ")
 
-for filename in os.listdir(Folder):
-    Filepath = os.path.join(Folder, filename)
-    print(Filepath)
+for filename in os.listdir(folder):
+    filepath = os.path.join(folder, filename)
 
+    if os.path.isfile(filepath):
+        try:
+            with open(filepath, 'r', encoding='utf-8') as fp:
+                for line in fp:
+                    if search in line:
+                        asw.add((line.strip(), filepath))
+        except Exception:
+            continue
+
+for line, path in asw:
+    print(f"{line}\n[Found in: {path}]\n")
+
+#2 Get file info
+with open("file.txt", "r") as parsingfile:
+    filecontent = parsingfile.read()
+
+letterscount = sum(map(str.isalpha, filecontent))
+wordscount = len(filecontent.split())
+linescount = filecontent.count("\n") + (1 if filecontent else 0)
+print(f"""File contains:
+      {letterscount} letters
+      {wordscount} words
+      {linescount} lines""")
+
+#3 Pathlib module
